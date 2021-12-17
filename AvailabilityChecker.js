e3e746617fba4acf6b95810726b8c95d5407ea1c
Astrid <astridbe@student.chalmers.se>
2021-12-17 15:47:20 +0100

    add mocked data and remove timeslots at breaks
    Co-Authored-By: emiant's avataremiant <emiant@student.chalmers.se>

--- AvailabilityChecker.js
+++ AvailabilityChecker.js
@@ -13,7 +13,7 @@ const mqtt = require("./Mqtt");
 
 /**  Subscribed topics */
 const checkBookingTopic = "Team5/Dentistimo/Check/Booking"; //Booking information from frontend - confirm - should include issuance
-
+const getTimeslotTopic = "/Team5/Dentistimo/TimeSlots"
 /**  Published topics */
 const bookingValidatedTopic = "Team5/Dentistimo/Booking/Create/Request"; // Forward to Booking Handler
 const bookingRejectedTopic = "Team5/Dentistimo/Reject/Booking"; // Message to frontend
@@ -23,9 +23,129 @@ const database = require("./Database");
 const booking = require("./Models/booking.js");
 
 mqtt.subscribeToTopic(checkBookingTopic); //TODO: Update to the topic from frontend
-//mqtt.subscribeToTopic(getTimeslotTopic);
+mqtt.subscribeToTopic(getTimeslotTopic);
 
 /**  Listen to messages below */
+var data = {
+  "clinicId": "",
+  "timeslots":[
+    {
+      "_id": "61bc57e6ce0d87512e7329ab",
+      "start": "9:00",
+      "end": "9:30",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329ac",
+      "start": "9:30",
+      "end": "10:00",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329ad",
+      "start": "10:00",
+      "end": "10:30",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329ae",
+      "start": "10:30",
+      "end": "11:00",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329af",
+      "start": "11:00",
+      "end": "11:30",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b0",
+      "start": "11:30",
+      "end": "12:00",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b1",
+      "start": "12:00",
+      "end": "12:30",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b2",
+      "start": "12:30",
+      "end": "13:00",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b3",
+      "start": "13:00",
+      "end": "13:30",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b4",
+      "start": "13:30",
+      "end": "14:00",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b5",
+      "start": "14:00",
+      "end": "14:30",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b6",
+      "start": "14:30",
+      "end": "15:00",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b7",
+      "start": "15:00",
+      "end": "15:30",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b8",
+      "start": "15:30",
+      "end": "16:00",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329b9",
+      "start": "16:00",
+      "end": "16:30",
+      "available": 3,
+      "date": "Thu Dec \r\n02 2021"
+    },
+    {
+      "_id": "61bc57e6ce0d87512e7329ba",
+      "start": "16:30",
+      "end": "17:00",
+      "available": 3,
+      "date": "Thu Dec 02 2021"
+    }
+  ]
+  }
+
+console.log(data);
+const test = saveTimeslotsAsArray(data);
 
 mqtt.client.on("message", function (topic, message) {
   switch (topic) {
@@ -35,6 +155,7 @@ mqtt.client.on("message", function (topic, message) {
 
       break;
     case getTimeslotTopic:
+      //saveTimeslotsAsArray(JSON.parse(message));
       //method call
       break;
     default:
@@ -42,7 +163,7 @@ mqtt.client.on("message", function (topic, message) {
   }
 });
 
-/**  Functions */
+/*  Check booking Functions */
 
 var issuanceQueue = new MinPriorityQueue({
   priority: (booking) => booking.issuance,
@@ -100,3 +221,33 @@ const rejectBooking = (booking) => {
   client.publish(bookingRejectedTopic, JSON.stringify(booking));
   console.log("Booking rejected:" + bookingInfo);
 };
+
+
+/* Check timeslots functions */
+
+function saveTimeslotsAsArray (message) {
+    var timeslot = message;
+    //let timeslots = [];
+    //timeslots.push(timeslot);
+    console.log(timeslot); 
+    updateBreaks(timeslot);
+    // Get non-filter timeslots in array
+};
+
+
+
+function updateBreaks (timeslots) {
+
+  breaks(timeslots);
+  const result = timeslots.timeslots.filter(item => item.start !== '12:00' && item.start !== '12:30' && item.start !== '10:00' && item.start !== '15:00');
+
+  console.log('after filter')
+  console.log(result);
+  // timeslots = fikaBreak(timeslots); 
+  // After this check bookings 
+};
+
+// check bookings 
+// update availability (if 0 remove)
+
+// Vi skickar timeslotsen till frontend
