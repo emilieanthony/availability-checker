7c8786e8a4258bbeca18959a16d94c7d04f07110
Astrid <astridbe@student.chalmers.se>
2021-12-22 16:42:43 +0100

    add error handling to database

--- Database.js
+++ Database.js
@@ -3,5 +3,10 @@ const mongoose = require("mongoose");
 const databaseURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dentistimo0.vd9sq.mongodb.net/Dentistimo`
 
 mongoose.connect(databaseURI, function(){
+    if (err) {
+        console.error(`Failed to connect to MongoDB with URI: ${databaseURI}`);
+        console.error(err.stack);
+    }
+
     console.log('Connected to database')
 })
