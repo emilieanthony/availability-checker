0c824d758630ed8800ef8bc32f0f8834678f3215
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2022-01-02 08:22:04 +0100

    Last will message is coded allowing reaction on disconnection as well as disconnecting the component via user input

--- Mqtt.js
+++ Mqtt.js
@@ -4,7 +4,13 @@ const mqtt = require('mqtt')
 /** Different MQTT servers */
 const LOCALHOST = 'mqtt://127.0.0.1:1883'; //TODO: fill with the local mqtt address
 const HOST = 'mqtt://test.mosquitto.org'; //mosquitto test server address
-const options = {clientId:'Dentistimo Team5 - Availability Checker n°' + Math.random().toString(16).substr(2, 8)}
+const options = {clientId:'Dentistimo Team5 - Availability Checker n°' + Math.random().toString(16).substr(2, 8),
+    will: {
+        topic: "Team5/Dentistimo/AvailabilityChecker/LastWill",
+        payload: "Availability Checker has been disconnected from the system",
+        qos: 1
+    }
+}
 
 /** Connects to the servers defined in the constants above */
 const client = mqtt.connect(LOCALHOST, options) //Change the parameter between HOST or LOCALHOST if you want to connect to the mosquitto test broker or a local broker. For local, mosquitto needs to be installed and running
@@ -30,6 +36,36 @@ module.exports.subscribeToTopic = function (topic){
             console.log(err.message);
         }
     })
+}
+
+/**
+ * Subscribe of all topics in the array and prints in the console a confirmation message for each topic.
+ * @param arrayOfTopics
+ */
+module.exports.subscribeToAll = function (arrayOfTopics){
+    arrayOfTopics.forEach(topic => {
+            client.subscribe(topic,function (error) {
+                if (error) {
+                    console.log(error)
+                } else {
+                    console.log('Subscribe to topic ' + topic)
+                }
+            })
+        }
+    )
+}
+
+/**
+ * Unsubscribes to the topics contained in the array and print in the console a confirmation message. Then, disconnects from the mqtt broker
+ * @param arrayOfTopics
+ */
+module.exports.disconnect = function (arrayOfTopics){
+    arrayOfTopics.forEach(topic => {
+            client.unsubscribe(topic, console.log('Unsubscribing to topic ' + topic))
+        }
+    )
+    client.end(false)
+    console.log('Disconnecting from MQTT broker.')
 }
 
 /**
